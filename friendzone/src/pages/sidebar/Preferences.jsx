ea8befeddea91a5aa1afe4b25786d9268546bae2
monica-lucaci <138821338+monica-lucaci@users.noreply.github.com>
2023-11-15 10:19:50 +0100

    add style to card

--- friendzone/src/pages/sidebar/Preferences.jsx
+++ friendzone/src/pages/sidebar/Preferences.jsx
@@ -3,9 +3,12 @@ import Tabs from "../../components/tabs/Tabs";
 import CircleComponent from "../../components/animationCircles/CircleComponent";
 
 const Preferences = () => {
-  return <div className="text-gray-400 bg-transparent relative w-full">Preferences
-  <CircleComponent />
-  </div>;
+  return (
+    <div className="text-gray-400 bg-transparent relative w-full">
+      Preferences
+      <CircleComponent />
+    </div>
+  );
 };
 
 export default Preferences;
