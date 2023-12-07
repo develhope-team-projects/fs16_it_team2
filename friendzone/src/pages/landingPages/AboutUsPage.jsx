9f1d72fd0a4cd457eeef6e6304590b70687b1cca
Eleonora Melodia <eleonoramelodia92@gmail.com>
2023-12-07 11:21:46 +0100

    LP: about us finalized

--- friendzone/src/pages/landingPages/AboutUsPage.jsx
+++ friendzone/src/pages/landingPages/AboutUsPage.jsx
@@ -6,23 +6,21 @@ import imgele3 from "../../assets/images/landingPage/about-us/reggeaton.png";
 import imgMiriana1 from "../../assets/images/landingPage/about-us/miriana.jpg";
 import imgMiriana2 from "../../assets/images/landingPage/about-us/japan.png";
 import imgMiriana3 from "../../assets/images/landingPage/about-us/books.png";
-import imgLuigi1 from "../../assets/images/landingPage/about-us/profile_pic.jpg"
-import imgLuigi2 from "../../assets/images/landingPage/about-us/hardware.png"
-import imgLuigi3 from "../../assets/images/landingPage/about-us/electronicmusic.png"
+import imgLuigi1 from "../../assets/images/landingPage/about-us/profile_pic.jpg";
+import imgLuigi2 from "../../assets/images/landingPage/about-us/hardware.png";
+import imgLuigi3 from "../../assets/images/landingPage/about-us/electronicmusic.png";
 import imgDesiré1 from "../../assets/images/landingPage/about-us/desiré.png";
 import imgDesiré2 from "../../assets/images/landingPage/about-us/dog.png";
 import imgDesiré3 from "../../assets/images/landingPage/about-us/orologio.png";
 import imgMonica1 from "../../assets/images/landingPage/about-us/monica.jpg";
 import imgMonica2 from "../../assets/images/landingPage/about-us/monicapesi.png";
 import imgMonica3 from "../../assets/images/landingPage/about-us/pittura.png";
-
-
+import Switcher from "../../components/darkMode/Switcher";
 import iceCream from "../../assets/images/icecreams/2icecrm.png";
 import { useEffect, useState } from "react";
-import background from "../../assets/images/landingPage/about-us/sfondo.jpg";
+import background from "../../assets/images/landingPage/about-us/aboutus2.mp4";
 import { IoIceCream } from "react-icons/io5";
 import DinamicColor from "../../components/dinamicColorIce/DinamicColor";
-
 
 const team2 = [
   {
@@ -30,12 +28,12 @@ const team2 = [
     role: "Junior Full-Stack Developer",
     iceCream: "Cream!",
     passions: "Loving dogs, and traditional wristwatches",
-    images: [imgDesiré1,imgDesiré2,imgDesiré3],
+    images: [imgDesiré1, imgDesiré2, imgDesiré3],
   },
   {
     name: "Eleonora Melodia",
     role: "Junior Full-Stack Developer",
-    iceCream: "Dark chocolate!",
+    iceCream: "Pistachio!",
     passions: "Crossfit and dancing reggeaton",
     images: [imgele1, imgele2, imgele3],
   },
@@ -56,51 +54,71 @@ const team2 = [
   {
     name: "Luigi Rauso ",
     role: "Junior Full-Stack Developer",
-    iceCream: "Nuts!",
+    iceCream: "Chocolate chip!",
     passions: "hardware fanatic, composing music",
     images: [imgLuigi1, imgLuigi2, imgLuigi3],
   },
 ];
 
 const AboutUsPage = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
   const [imageIndex, setImageIndex] = useState(0);
-  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+    setImageIndex(0); // Reset image index when leaving
+  };
 
   useEffect(() => {
     let interval;
 
-    // Cambio veloce delle immagini quando l'utente è in hover
-    if (isHovered) {
+    // Change images only if hovered and there are more than one image
+    if (hoveredIndex !== null && team2[hoveredIndex].images.length > 1) {
       interval = setInterval(() => {
-        setImageIndex((prevIndex) => (prevIndex + 1) % team2.length);
+        setImageIndex(
+          (prevIndex) => (prevIndex + 1) % team2[hoveredIndex].images.length
+        );
       }, 800);
     }
 
-    // Pulisce l'intervallo quando l'utente esce dall'hover o il componente si smonta
     return () => {
       clearInterval(interval);
     };
-  }, [isHovered]);
+  }, [hoveredIndex]);
 
   return (
     <div className="h-screen relative">
-        <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50 h-[35vh]" aria-hidden="true"></div>
-      <img
-        className="object-cover w-full h-[35vh]"
-        src={background}
-        alt="Hero Background"
-      />
+      <div
+        className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-100 h-[35vh]"
+        aria-hidden="true"
+      ></div>
+      <video autoPlay loop muted className="object-cover w-full h-[35vh]">
+        <source src={background} type="video/mp4" />
+        Il tuo browser potrebbe non supportare la riproduzione di video.
+      </video>
 
       <CircleComponent />
 
       <div className="container font-merienda mx-auto">
-        <Link
-          to="/"
-          className="flex items-center mb-4 left-2 z-10 absolute text-purple-500 hover:underline"
-        >
-          <ion-icon name="home" className="mr-2"></ion-icon>
-          Back to Home
-        </Link>
+        <ul className="absolute inline-flex mt-2 gap-4 left-10 top-2 z-10 ">
+          <li>
+            <Link
+              to="/"
+              className="flex m-2 items-center text-purple-500 hover:underline"
+            >
+              <ion-icon name="home" className="mr-2"></ion-icon>
+              Back to Home
+            </Link>
+          </li>
+          <li>
+            <Switcher />
+          </li>
+        </ul>
+
         <div className="absolute p-6 justify-center top-2 z-10">
           <h1 className="mb-6 text-4xl font-merienda text-white  font-extrabold ">
             About Us
@@ -108,8 +126,9 @@ const AboutUsPage = () => {
 
           <p className="mb-8  font-merienda text-white text-xl font-bold ">
             Welcome to the About Us page of Friendzone! <br />
-            We are a group of passionate about ice cream <ion-icon name="ice-cream-outline"></ion-icon>  and code on a journey to
-            master the art of web development . <br />
+            We are a group of passionate about ice cream{" "}
+            <ion-icon name="ice-cream-outline"></ion-icon> and code on a journey
+            to master the art of web development . <br />
             Our goal is to unite people, where technology is often accused of
             dividing. <br />
             We want to allow a tool within everyone's reach that can allow you
@@ -129,28 +148,46 @@ const AboutUsPage = () => {
           Meet the Ice Cream Team:
         </h2>
 
-        <div className="grid grid-cols-2 place-items-center flex-wrap gap-8">
-          {/* Itera attraverso gli oggetti nell'array 'team2' */}
+        <div className="grid grid-cols-2 place-items-center flex-wrap gap-8 mx-auto">
           {team2.map((member, index) => (
             <div
               key={index}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => {
-                setIsHovered(false);
-                // Resetta l'indice dell'immagine quando l'utente esce dall'hover
-                setImageIndex(0);
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+              className="w-[350px] flex-col justify-center bg-white rounded-lg shadow-md"
+              style={{
+                boxShadow: `0 0 10px rgba(0, 0, 255, 0.7)${
+                  hoveredIndex === index ? ", 0 0 20px 5px purple" : ""
+                }`,
               }}
-              className="p-6 w-[300px] bg-gradient-to-t from-[#f3e3ee] to-[#a6c1ee] flex-col justify-center mr-2 bg-white rounded-lg shadow-md"
             >
-              <img
-                className="w-[250px] h-[300px] transition-opacity duration-500"
-                src={member.images[imageIndex]}
-                alt="team member photo"
-              />
-              <h2 className="mb-2 text-lg font-semibold">{member.name}</h2>
-              <h3 className="text-gray-600">{member.role}</h3>
-              <h4>My loved taste: {member.iceCream} <span> <DinamicColor>{<IoIceCream/>}</DinamicColor> </span> </h4>
-              <p>Passions: {member.passions} </p>
+              <div className="flex-col justify-center ms-6">
+               
+                <div className="p-4">
+                   <img
+                  className="w-[260px] h-[300px] transition-opacity duration-500"
+                  src={
+                    hoveredIndex === index
+                      ? member.images[imageIndex]
+                      : member.images[0]
+                  }
+                  alt="team member photo"
+                />
+                  <h2 className="mb-2 p-2 text-lg font-semibold">
+                    {member.name}
+                  </h2>
+                  <h3 className="p-2 text-gray-600">{member.role}</h3>
+                  <h4>
+                    My loved taste: {member.iceCream}
+                    <span className="inline-block">
+                      <DinamicColor>
+                        <IoIceCream />
+                      </DinamicColor>
+                    </span>
+                  </h4>
+                  <p className="p-2">Passions: {member.passions} </p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
